efe12b1520e25b836eb98229f9050b8e2d954e8e
DylanBrss <dylanbrisseau79@gmail.com>
2023-01-30 17:37:00 +0100

    finition des parties shop cart + fiche de chaque produit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import "./header.css";
 
 import { motion } from "framer-motion";
@@ -9,6 +9,7 @@ import logo from "../../assets/images/eco-logo.png"
 import userIcon from "../../assets/images/user-icon.png"
 
 import { Container, Row } from 'reactstrap';
+import { useSelector } from 'react-redux';
 
 const nav__links = [
   {
@@ -28,7 +29,10 @@ const nav__links = [
 const Header = () => {
 
   const headerRef = useRef(null);
+  const totalQuantity = useSelector(state => state.cart.totalQuantity);
+
   const menuRef = useRef(null);
+  const navigate = useNavigate()
 
   const stickyHeaderFunc = () => {
     window.addEventListener('scroll', () => {
@@ -46,7 +50,11 @@ const Header = () => {
     return () => window.removeEventListener('scroll', stickyHeaderFunc);
   });
 
-  const menuToggle = () => menuRef.current.classList.toggle('active__menu')
+  const menuToggle = () => menuRef.current.classList.toggle('active__menu');
+
+  const navigateToCart = () => {
+    navigate('/cart')
+  }
 
   return (
     <header className="header" ref={headerRef}>
@@ -77,9 +85,9 @@ const Header = () => {
                 <i class="ri-heart-line"></i>
                 <span className="badge">1</span>
               </span>
-              <span className='cart__icon'>
+              <span className='cart__icon' onClick={navigateToCart}>
                 <i className="ri-shopping-bag-line"></i>
-                <span className="badge">2</span>
+                <span className="badge">{totalQuantity}</span>
               </span>
               <span>
                 <motion.img whileTap={{ scale: 1.2 }} src={userIcon} alt="" />
